ff411a53f1e3ac87e91639b879c685d6172ffd13
alaansabu <alansabu6046@gmail.com>
2025-11-28 16:16:53 +0530

    login partial commit

---
+++ projects/ecomm/client/src/main.ts
@@ -0,0 +1,44 @@
+interface values{
+
+name:string
+pass:string
+mail:string
+
+}
+
+function getUserData(data:values):void{
+
+
+
+try {
+    localStorage.setItem("student_login",JSON.stringify(data))
+
+} catch (error) {
+    console.log(error);
+    
+}
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const saveDraft =  document.querySelector(".S_btn") as HTMLButtonElement;
+  const getname =  document.querySelector(".name") as HTMLInputElement;
+  const getpass =  document.querySelector(".pass") as HTMLInputElement;
+  const getmail =  document.querySelector(".mail") as HTMLInputElement;
+
+  saveDraft.addEventListener("click",(event:MouseEvent)=>{
+    event.preventDefault()
+    console.log("button click");
+
+    const loginDetails:values ={
+      name:getname.value,
+      pass:getpass.value,
+      mail:getmail.value
+    }
+    getUserData(loginDetails)
+  })
+})
+
+
+
+
